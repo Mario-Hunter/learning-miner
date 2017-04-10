301d4631dc3ec8446142bc11116d17954ec6fd64
Mario Hunter <Mario Hunter>
2017-04-10 16:16:39 +0200

    Course Controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -7,77 +7,96 @@ use App\Course;
 
 class CourseController extends Controller
 {
-<<<<<<< HEAD
-=======
 
-    public function __construct()
-    {
-        $this->middleware('auth')->except(['show', 'index']);
-    }
+ public function __construct()
+ {
+    $this->middleware('auth',['except' => ['index','show']]);
+}
+public function index(){
+   $courses = Course::latest()
+   ->get();
 
-
-    public function index()
-    {
-    	$courses = Course::latest()->get();
->>>>>>> refs/remotes/origin/master
-
-    public function __construct()
-    {
-        $this->middleware('auth')->except(['show', 'index']);
-    }
+   return view('courses.index',compact('courses'));
+}
 
 
-<<<<<<< HEAD
-    public function index()
-    {
-      $courses = Course::latest()->get();
+public function create(){
+   return view('courses.create');
+}
 
-      return view('courses.index',compact('courses'));
-    }
+public function store(){
+   $this -> validate(request(),[
+      'url' => 'required',
+      'tags' => 'required',
+      'name' =>'required'
 
 
-    public function create()
-    {
-      return view('courses.create');
-    }
+      ]);
 
-    public function store()
-    {
-      $this -> validate(request(),[
-                                  'url' => 'required',
-                                  'tags' => 'required',
-                                  'name' =>'required'
-                                    ]);
+   auth()->user()->publish(
+     new Course(request(['url','name']))
 
-      auth()->user()->publish(new Course(request(['url','name'])));
-=======
-    public function create()
-    {
-    	return view('courses.create');
-    }
+     );
 
-    public function store()
-    {
-    	$this -> validate(request(),[
-    		                          'url' => 'required',
-    		                          'tags' => 'required',
-    		                          'name' =>'required'
-                                    ]);
+   return redirect('/courses');
 
-    	auth()->user()->publish(new Course(request(['url','name'])));
->>>>>>> refs/remotes/origin/master
+}
 
-      return redirect('/courses');
 
-    }
 
-    public function show(Course $course)
-    {
-<<<<<<< HEAD
-      return view('courses.show',compact('course'));
-=======
-    	return view('courses.show',compact('course'));
->>>>>>> refs/remotes/origin/master
-    }
+public function show(Course $course){
+   return view('course.show',compact('course'));
+}
+
+}
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Course;
+
+class CourseController extends Controller
+{
+
+ public function __construct()
+ {
+    $this->middleware('auth',['except' => ['index','show']]);
+}
+public function index(){
+   $courses = Course::latest()
+   ->get();
+
+   return view('courses.index',compact('courses'));
+}
+
+
+public function create(){
+   return view('courses.create');
+}
+
+public function store(){
+   $this -> validate(request(),[
+      'url' => 'required',
+      'tags' => 'required',
+      'name' =>'required'
+
+
+      ]);
+
+   auth()->user()->publish(
+     new Course(request(['url','name']))
+
+     );
+
+   return redirect('/courses');
+
+}
+
+
+
+public function show(Course $course){
+   return view('course.show',compact('course'));
+}
 
 }
