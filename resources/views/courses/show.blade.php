faca3264e3f28a8bfe2c95d68512559263c33385
akram mousa <akram mousa>
2017-05-20 20:32:57 +0200

    home

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,10 +1,12 @@
-@extends('layouts.master')
+ @extends('layouts.master')
 
 @section('content')
 
 <div class="container">
 	<div class="row">
-		@include('courses.course')	
+	
+		@include('courses.course')
+
 	</div>
 	<div class="row">
 		<div class="col-md-12 text-center">
