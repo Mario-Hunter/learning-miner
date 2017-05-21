4b93d1c071b654c5c574f02b0d27e500a428b6f2
Mario_Hunter <mario.hunter.std@alexu.edu.eg>
2017-05-21 02:49:58 +0200

    mail blade

--- resources/views/mails/FollowNotification.blade.php
+++ resources/views/mails/FollowNotification.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Hello, {{$userfollowed-> first_name}},
 
-{{$userfollowing->first_name}} {{$userfollowing->user->last_name}} has just followed you on your e-miner.
+{{$userfollowing->first_name}} {{$userfollowing->last_name}} has just followed you on your e-miner.
 
 @component('mail::button', ['url' => URL::to('users/' .$userfollowing->id)  ])
 Visit his profile.
